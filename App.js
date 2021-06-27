9880559cf4b6be1596fe0929faba3bc4f48e79e7
lyjelin <u3560393@connect.hku.hk>
2021-06-27 16:21:42 +0800

    Arrays

---
+++ App.js
@@ -0,0 +1,20 @@
+const mon = "mon";
+const tue = "tue";
+const wed = "wed";
+const thu = "thu";
+const fri = "fri";
+const sat = "sat";
+const sun = "sun";
+
+const daysOfWeek = [mon, tue, wed, thu, fri, sat];
+
+const nonsense = [1, 2, "hello", false, null, true, undefined];
+
+// Print Array
+console.log(daysOfWeek);
+
+// Get Item from Array
+console.log(daysOfWeek[2])
+
+// Add one more day to the array
+daysOfWeek.push("sun");
