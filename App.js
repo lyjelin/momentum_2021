43423412a3379e05ec99a460974a4b7d540e44c1
lyjelin <u3560393@connect.hku.hk>
2021-06-28 23:00:33 +0800

    Functions

--- App.js
+++ App.js
@@ -1,13 +1,28 @@
+function sayHello( nameOfPerson, age ) {
+    console.log("Hello my name is "+nameOfPerson+" and I am "+age+".");
+}
+
+sayHello("Elin", 22);
+sayHello("Dal", 23);
+sayHello("Lynn", 21);
+
+function plus(a, b){
+    console.log(a+b);
+}
+
+plus(15, 13);
+
+function divide(a, b){
+    console.log(a/b);
+}
+
+divide(12, 2);
+
 const player = {
-    name: "elin",
-    points: 10,
-    happy: true
+    name: "Elin",
+    sayHello: function(personName){
+        console.log("Hello! "+personName + ". Nice to meet you!");
+    },
 };
 
-console.log(player.name);
-// is same as 
-console.log(plauer["name"]);
-// we can update the object variable
-player.points = player.points + 15;
-//we can even add new variable 
-player.major = "Computer Engineering";
+player.sayHello("Lynn");
