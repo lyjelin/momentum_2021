a2f839ba21fa2d98dafe5803bad1df2a849f9a01
lyjelin <u3560393@connect.hku.hk>
2021-06-30 21:30:15 +0800

    Conditionals Part 1

--- App.js
+++ App.js
@@ -1,26 +1,3 @@
-const age = 96;
+const age = parseInt(prompt("How old are you"));
 
-function calculateKrAge(ageOfForeigner){
-    return ageOfForeigner + 2;
-}
-
-const krAge = calculateKrAge(age);
-
-console.log(krAge);
-
-const calculator = {
-    plus: function (a, b){
-        return a + b;
-    },
-    minus: function (a, b){
-        return a - b;
-    },
-    divide: function (a, b){
-        return a / b;
-    },
-    multiple: function (a, b) {
-        return a * b;
-    },
-};
-
-const plusResult = calculator.plus(2, 3);
+console.log(age);
